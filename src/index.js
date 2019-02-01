621247e86c76b8a1b13801a32c00764df6b37034
jv2301 <jv2301@gmail.com>
2019-02-01 18:39:15 -0400

    Created production scripts

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
-import numeral from 'numeral';
+import {getUsers, deleteUser} from './api/usersApi';
 import './index.css';
 
-const courseValue = numeral(1000).format('$0,0.00');
-console.log(`I would pay ${courseValue} for this awesome course!`);
+getUsers().then(result => {
+  let usersBody = "";
+  result.forEach(user => {
+    usersBody += `<tr>
+      <td><a href="#" data-id=${user.id} class="deleteUser">Delete</a></td>
+      <td>${user.id}</td>
+      <td>${user.firstName}</td>
+      <td>${user.lastName}</td>
+      <td>${user.email}</td>
+    </tr>`;
+    global.document.getElementById('users').innerHTML = usersBody;
+  });
+});
+
+const deleteLinks = global.document.getElementByClassName('deleteUser');
+
+Array.from(deleteLinks, link => {
+  link.onClick = event => {
+    const element = event.target;
+    event.preventDefault();
+    deleteUser(element.attributes['data-id'].value);
+    const row = element.parentNode.parentNode;
+    row.parentNode.removeChild(row);
+  }
+})
